bec53b1592ea7b7116f1c4481f41ac33074686d9
BlastoiseBlue <emmet@stanevich.com>
2017-11-21 13:51:10 -0500

    Buils Driver and Student classes

--- src/project/Driver.java
+++ src/project/Driver.java
@@ -1,10 +1,45 @@
 package project;
 
 public class Driver {
+	// Step 3:
+	public static int countThe(String[] s1) {
+		int numThe = 0;
+		for (int i = 0; i < s1.length; i++) {
+			if (s1[i].equalsIgnoreCase("the"))
+				numThe = numThe + 1;
+		}
+		return numThe;
+	}
+
+	// Step 4:
+	public static boolean compareOdd(float[] arr1, float[] arr2) {
+		if (arr1.length == arr2.length) {
+			for (int i = 1; i < arr1.length; i = i + 2) {
+				if (arr1[i] != arr2[i])
+					return false;
+			}
+			return true;
+		}
+		return false;
+	}
 
 	public static void main(String[] args) {
-		//Step 2:
-		
+		// Step 2:
+		String[] s = { "The", "coach", "urged", "the", "team", "to", "press", "on", "and", "to", "score", "again",
+				"before", "the", "end", "of", "the", "third", "quarter" };
+		// Step 3:
+		System.out.println(countThe(s));
+		// Step 4:
+		float[] arr1f = { 1.0f, 2.0f, 3.0f };
+		float[] arr2f = { 44.5f, 2.0f, 4.0f };
+		System.out.println(compareOdd(arr1f, arr2f));
+		// Step 5:
+		Student[] students = new Student[3];
+		students[0] = new Student("Emmet", 3.5, 18);
+		students[1] = new Student("John", 3.6, 20);
+		students[2] = new Student("Steve", 3.2, 19);
+		// Step 6:
+		System.out.println("Best student: " + Student.findBestStudent(students));
 	}
 
 }
--- src/project/Student.java
+++ src/project/Student.java
@@ -1,5 +1,31 @@
 package project;
 
+//Step 5:
 public class Student {
-	
+	String name;
+	double gpa;
+	int age;
+
+	public String toString() {
+		return (name + ", " + age + ", " + gpa);
+	}
+
+	public Student(String n, double g, int a) {
+		name = n;
+		gpa = g;
+		age = a;
+	}
+
+	// Step 6:
+	public static String findBestStudent(Student[] s) {
+		double highestGPA = 0;
+		int bestStudent = 0;
+		for (int i = 0; i < s.length; i++) {
+			if (s[i].gpa > highestGPA) {
+				highestGPA = s[i].gpa;
+				bestStudent = i;
+			}
+		}
+		return s[bestStudent].name;
+	}
 }
